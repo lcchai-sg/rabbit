278604ba6c7cb3bffd92b1aec7248970e247b4be
LC Chai <leongchoi@ieplsg.com>
2021-04-24 11:26:41 +0800

    competitors output

---
+++ scrape/fix/fix_assets/u_ptime_no_assets.js
@@ -0,0 +1,24 @@
+const u = [
+    "https://www.prestigetime.com/Breguet-Reine-de-Naples-Power-Reserve-8908ba-52-964-d00d3L.html",
+    "https://www.prestigetime.com/Breguet-Reine-de-Naples-Power-Reserve-8908bb-52-964-d00d3L.html",
+    "https://www.prestigetime.com/Breguet-Reine-de-Naples-Power-Reserve-8908br-5t-964-d00d3L.html",
+    "https://www.prestigetime.com/Breitling-Chronomat-Automatic-36-a10380611L1p1.html",
+    "https://www.prestigetime.com/Breitling-Chronomat-Automatic-36-a10380611a1p1.html",
+    "https://www.prestigetime.com/Breitling-Chronomat-Automatic-36-a10380611c1p1.html",
+    "https://www.prestigetime.com/Bulgari-Serpenti-Tubogas-35mm-103433.html",
+    "https://www.prestigetime.com/Bulgari-Serpenti-Tubogas-35mm-103524.html",
+    "https://www.prestigetime.com/Longines-Conquest-Quartz-Ladies-L3-380-4-76-6-L33804766.html",
+    "https://www.prestigetime.com/Patek-Philippe-Aquanaut-Automatic-5167a-001.html",
+    "https://www.prestigetime.com/preowned/Audemars-Piguet-Edward-Piguet-Automatic-15121bc.oo.a002cr.02-48907",
+    "https://www.prestigetime.com/preowned/Breitling-Chronomat-B01-Chronograph-44-cb0110121b1c1-53896",
+    "https://www.prestigetime.com/preowned/Girard-Perregaux-Laureato-Skeleton-Automatic-42mm-81015-11-001-11a-53895",
+    "https://www.prestigetime.com/preowned/Omega-Seamaster-Diver-300m-Co-Axial-Master-Chronometer-42mm-210.20.42.20.03.001-49050",
+    "https://www.prestigetime.com/preowned/Oris-Big-Crown-ProPilot-Date-41mm-01_751_7697_4164-07_5_20_17FC-53755",
+    "https://www.prestigetime.com/preowned/Oris-Big-Crown-ProPilot-Date-41mm-01_751_7697_4164-07_8_20_19-53766",
+    "https://www.prestigetime.com/preowned/Oris-Big-Crown-ProPilot-Day-Date-45mm-01_752_7698_4063-07_5_22_06FC-53757",
+    "https://www.prestigetime.com/preowned/Rolex-Datejust-41mm-126300_Blue_Index_Jubilee-54090",
+    "https://www.prestigetime.com/preowned/Tag-Heuer-Aquaracer-Automatic-LIMITED-41mm-wbd218b.fc6446-45753",
+    "https://www.prestigetime.com/preowned/Tudor-Pelagos-42mm-m25600tn-0001-53918"
+];
+
+module.exports = u;
